8cdae67e901ce06481416bae40982684b2b25d30
Nikita Sergeev <nikitasergeev1357@gmail.com>
2024-04-26 19:01:15 +0300

    Update: urls.py, views.py, index.html

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
+from .models import Todo
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    todos = Todo.objects.all()
+    return render(request, 'todolist/index.html', {'todo_list': todos,
+                                                   'title': 'Главная страница',
+                                                   })
+
+
+@require_http_methods(['POST'])
+def add(request):
+    title = request.POST['title']
+    todo = Todo(title=title)
+    todo.save()
+    return redirect('index')
+
+
+def update(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.is_complete = todo.is_complete
+    todo.save()
+    return redirect('index')
+
+
+def delete(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.delete()
+    return redirect('index')
